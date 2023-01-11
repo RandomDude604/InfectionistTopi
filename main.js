771f118e4dc6d7d900465c94b33138c2101dbd81
RandomDude604 <70555524+RandomDude604@users.noreply.github.com>
2023-01-11 05:57:50 +0000

    Added Header thing at the top and added your current time

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function firstMessage() {
     document.getElementById("first-message").innerHTML = "Feel Free to Explore! I'm still learning as I'm making this sorry about the bugs and all!";
   }
+
+  setInterval(myTime, 1000);
+function myTime() {
+
+
+  const d = new Date();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString();
+}
