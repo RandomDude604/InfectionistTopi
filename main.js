89987502ec56bf3af8157fd9f0b0231e7bfcec11
RandomDude604 <70555524+RandomDude604@users.noreply.github.com>
2023-01-14 05:30:41 +0000

    added a new block on homepage

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ function myTime() {
   const d = new Date();
   document.getElementById("time").innerHTML = d.toLocaleTimeString();
 }
+
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark'){
+      setTheme('theme-light');
+  } else {
+      setTheme('theme-dark');
+  }
+}
+
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-dark');
+  } else {
+      setTheme('theme-light');
+  }
+})();
